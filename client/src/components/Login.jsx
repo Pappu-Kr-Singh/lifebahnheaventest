afe308292b9a5fe275c5d0059343e89897a50bfe
Pappu Singh <pappu.singh159687@gmail.com>
2024-10-26 21:33:15 +0530

    tesssssttttttt

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/v1/users/login",
+        "https://lifebahnheavenserver-ochre.vercel.app/api/v1/users/login",
         formData,
         {
           headers: {
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -43,7 +43,7 @@ const SignUp = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/v1/users/register",
+        "https://lifebahnheavenserver-ochre.vercel.app/api/v1/users/register",
         data,
         {
           headers: {
